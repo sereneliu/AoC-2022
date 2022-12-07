73ef942f94d14b61b5dd5bf7c736f3ac638346bb
Serene <github@serenely.me>
2022-12-06 22:35:52 -0500

    Completed both parts of day 6

--- day06.py
+++ day06.py
@@ -1,1 +1,9 @@
-puzzle_input = open('day6.txt', 'r').read().split('\n')
+puzzle_input = open('day06.txt', 'r').read()
+
+def find_marker(datastream_buffer, no_chars):
+    for i in range(len(datastream_buffer)):
+        if len(set(datastream_buffer[i:i + no_chars])) == no_chars:
+            return i + no_chars
+
+print(find_marker(puzzle_input, 4)) # 1155
+print(find_marker(puzzle_input, 14)) # 2789
