a2aab132e06d6c56fe60f9322d63c49f327ea513
Serene <github@serenely.me>
2023-04-11 23:45:40 -0400

    Completed part 2 of day 8, but part 1 no longer working

--- day08.py
+++ day08.py
@@ -1,5 +1,5 @@
-puzzle_input = open('day08.txt', 'r').read().split('\n')
-sample_input = open('day08_sample.txt', 'r').read().split('\n')
+from aocd import get_data
+puzzle_input = get_data(day=8, year=2022).split('\n')
 
 def down(x, y):
      return x, y + 1
@@ -30,63 +30,66 @@ def find_tree_visibility(tree_coords, height, max_coord):
     x, y = tree_coords
     side_visible = True
     scenic_score = 0
-    if x == 0 or x == max_coord or y == 0 or y == max_coord:
-        return True
+    scenic_scores = []
+    multiplied_score = 1
+    if y == max_coord:
+        scenic_score = 1
     while y != max_coord:
        x, y = down(x, y)
-       # print((x, y), trees[(x, y)])
        scenic_score += 1
        if trees[(x, y)] >= height:
            side_visible = False
            break
-    if side_visible == True:
-        return True
+    scenic_scores.append(scenic_score)
     x, y = tree_coords
     side_visible = True
+    scenic_score = 0
+    if y == 0:
+        scenic_score = 1
     while y != 0:
         x, y = up(x, y)
-        # print((x, y), trees[(x, y)])
         scenic_score += 1
         if trees[(x, y)] >= height:
            side_visible = False
            break
-    if side_visible == True:
-        return True
+    scenic_scores.append(scenic_score)
     x, y = tree_coords
     side_visible = True
+    scenic_score = 0
+    if x == max_coord:
+        scenic_score = 1
     while x != max_coord:
         x, y = right(x, y)
-        # print((x, y), trees[(x, y)])
         scenic_score += 1
         if trees[(x, y)] >= height:
            side_visible = False
            break
-    if side_visible == True:
-        return True
+    scenic_scores.append(scenic_score)
     x, y = tree_coords
     side_visible = True
+    scenic_score = 0
+    if x == 0:
+        scenic_score = 1
     while x != 0:
         x, y = left(x, y)
-        # print((x, y), trees[(x, y)])
         scenic_score += 1
         if trees[(x, y)] >= height:
-           return False
-    return True, scenic_score
+            break
+    scenic_scores.append(scenic_score)
+    for score in scenic_scores:
+        multiplied_score = multiplied_score * score
+    return multiplied_score
 
 def find_visible_trees(tree_heights):
     store_tree_coords(tree_heights)
-    # print(trees)
     visible = 0
     scenic_score = 0
     max_scenic_score = 0
-    max_scenic_score = max(scenic_score, max_scenic_score)
     for tree, height in trees.items():
-        # print("start", tree, height, len(tree_heights[0]))
-        tree_visible, scenic_score = is_tree_visible(tree, height, len(tree_heights[0]) - 1)
-        if tree_visible:
+        scenic_score = find_tree_visibility(tree, height, len(tree_heights[0]) - 1)
+        if scenic_score > 0:
             visible += 1
-        # print("end", is_tree_visible(tree, height, len(tree_heights[0]) - 1))
+        max_scenic_score = max(scenic_score, max_scenic_score)
     return visible, max_scenic_score
 
-print(find_visible_trees(puzzle_input)) # 1672
-# print(find_visible_trees(sample_input))
+print(find_visible_trees(puzzle_input)) # Part 1: 1672, Part 2: 327180
